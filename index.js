a0c89398143a1abafe1c28067183e1845e90daf1
Steve Lacy <me@slacy.me>
2017-08-10 10:11:43 -0700

    cleanup, lets go es6

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 module.exports = function splatext (mapped, lib) {
-  const words = []
-  mapped.forEach((v, k) => {
-    const multi = v.split('.')
-    let word = ''
-    multi.forEach((v, k) => {
+  return mapped.map((v) =>
+    v.split('.').map((v) => {
       const split = v.split(':')
-      word += (lib[split[0]][split[1]])
-    })
-    words.push(word)
-  })
-  return words
+      return lib[split[0]][split[1]]
+    }).join('')
+  )
 }
--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 const splatext = require('.')
+const should = require('should')
 
 const lib = {
   a: [ 's' ],
@@ -7,6 +8,10 @@ const lib = {
   d: [ 'brown', 'jump', 'lazy', 'over', 'quick' ]
 }
 
-const mapped = [ 'c:2', 'd:4', 'd:0', 'c:1', 'd:1.a:0', 'd:3', 'c:2', 'd:2', 'c:0' ]
-
-console.log(splatext(mapped, lib).join(' '))
+describe('splatext', () => {
+  it('should parse a hash map', (done) => {
+    const mapped = [ 'c:2', 'd:4', 'd:0', 'c:1', 'd:1.a:0', 'd:3', 'c:2', 'd:2', 'c:0' ]
+    should(splatext(mapped, lib).join(' ')).equal('the quick brown fox jumps over the lazy dog')
+    done()
+  })
+})
